57b70ea433a9b39b6c1a552d855cf7a1bb24aea5
濯缨 <11262376+zhuoyyyy@user.noreply.gitee.com>
2025-03-11 18:58:28 +0800

    最新

--- Assets/Scripts/UI/Panels/LoginPanel.cs
+++ Assets/Scripts/UI/Panels/LoginPanel.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UIFrameWork;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LoginPanel : BasePanel
 {
+    private string name;
+    private string password;
+    
     public LoginPanel() : base(new UIType(UIInfo.LoginPanel))
     {
     }
@@ -17,8 +21,16 @@ public class LoginPanel : BasePanel
         
         FindComponent<Button>("Btn_Login").onClick.AddListener(OnLogin);
         FindComponent<Button>("Btn_Close").onClick.AddListener(Close);
+        FindComponent<TMP_InputField>("InputFieldName").onValueChanged.AddListener((value) =>
+        {
+            name = value;
+        });
+        FindComponent<TMP_InputField>("InputFieldPassword").onValueChanged.AddListener((value) =>
+        {
+            password = value;
+        });
     }
-
+    
     public void OnLogin()
     {
         //todo:使用netmanager发送协议登录，接收返回的数据,使用UserData.UpdateData更新
